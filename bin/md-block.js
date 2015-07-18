51d7ba36580eba6dae83e6b2930d9e4bf3dd86aa
clement <maboiteaspam@wanadoo.fr>
2015-07-18 19:43:17 +0800

    Add command line binaries to work with md content

---
+++ bin/md-block.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+var through2 = require("through2")
+var byline = require('byline')
+var fs = require('fs')
+var mdUtils = require('../index.js')
+var argv = require('minimist')(process.argv.slice(2));
+
+var input = argv._.length > 0 ? fs.createReadStream(argv._[0]) : process.stdin;
+var output = argv._.length > 1 ? fs.createWriteStream(argv._[1]) : process.stdout;
+var type = argv.t || argv.type || null;
+var content = argv.c || argv.content || null;
+
+input
+  .pipe(mdUtils.tokenizer())
+  .pipe(mdUtils.byBlock())
+  .pipe( content || type ? mdUtils.filter({type: type, content: content}) : through2.obj() )
+  .pipe( mdUtils.cliColorize())
+  .pipe(output)
+
+input.on('end', function(){
+  output.write('\n')
+})
---
+++ bin/md-paragraph.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+var through2 = require("through2")
+var byline = require('byline')
+var fs = require('fs')
+var mdUtils = require('../index.js')
+var argv = require('minimist')(process.argv.slice(2));
+
+var input = argv._.length > 0 ? fs.createReadStream(argv._[0]) : process.stdin;
+var output = argv._.length > 1 ? fs.createWriteStream(argv._[1]) : process.stdout;
+var content = argv.c || argv.content || null;
+
+input
+  .pipe(mdUtils.tokenizer())
+  .pipe(mdUtils.byParapgraph())
+  .pipe( content ? mdUtils.filter({content: content}) : through2.obj() )
+  .pipe( mdUtils.cliColorize())
+  .pipe(output)
+
+input.on('end', function(){
+  output.write('\n')
+})
